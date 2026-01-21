c22a7938ca966eecaba0a099527da6c33673ae4a
Ale8604 <alejandrosotelo080104@gmail.com>
2026-01-20 23:48:32 -0400

    subiendo tipo de variables

--- variables.tf
+++ variables.tf
@@ -45,11 +45,11 @@ variable "port" {
 
 variable "access_key" {
   description = "access_key"
-
+  type = string
   
 }
 
 variable "secret_key" {
   description = "secret_key"
-  
+  type = string
 }
